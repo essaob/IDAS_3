22207e1eb9ad59700b7c75ddcdf56bc0124fc7cd
MohamadNassar28 <mohamna2@ac.sce.ac.il>
2024-07-17 23:20:02 +0300

    profile view + test

--- myapp/tests.py
+++ myapp/tests.py
@@ -2,6 +2,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.test import TestCase, RequestFactory,Client, override_settings
+
+from IDAS_3.myapp.models import UserProfile
+
 
 class UserAuthTests(TestCase):
 
@@ -34,3 +38,32 @@ class UserAuthTests(TestCase):
             'password': 'testpassword123'
         })
         self.assertEqual(response.status_code, 302)  # Redirect after successful login
+
+
+class UserProfileModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='testpassword')
+    def test_create_user_profile(self):
+        # Check if a profile already exists for the user
+        if not UserProfile.objects.filter(user=self.user).exists():
+            UserProfile.objects.create(user=self.user)
+        else:
+            print("Profile already exists for the user.")
+
+        self.assertTrue(UserProfile.objects.filter(user=self.user).exists())
+    def test_user_profile_string_representation(self):
+        if not UserProfile.objects.filter(user=self.user).exists():
+            UserProfile.objects.create(user=self.user)
+        else:
+            print("Profile already exists for the user.")
+
+        user_profile = UserProfile.objects.get(user=self.user)
+        expected_string_representation = f'{self.user.username} Profile'
+
+
+
+
+class EditProfileViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.factory = RequestFactory()
